2f1bf58d1e3d916f13692e527d7733b6f94b51ff
Babacar10 <Diattafive@gmail.com>
2023-10-17 20:42:45 -0400

    Percentage Calculated

--- Percentage Script.py
+++ Percentage Script.py
@@ -30,13 +30,14 @@ def calculate_percentages(input_file, output_file, control_column_pairs):
     print(f"Percentage calculation completed. Results saved to {output_file}")
 
 # Example usage:
-input_file = 'PercentageCalculated.xlsx'  # Replace with your input Excel file
-output_file = 'PercentageCalculated1.xlsx'  # Replace with your desired output Excel file
+input_file = 'output.xlsx'  # Replace with your input Excel file
+output_file = 'PercentageCalculated.xlsx'  # Replace with your desired output Excel file
 
 # Define multiple pairs of control columns and associated columns
 control_column_pairs = [
-    {'control_column': 'total_senior_staff', 'columns': ['black_senior_staff']}
-
+    {'control_column': 'total_senior_staff', 'columns': ['black_senior_staff','asian_senior_staff', 'black_senior_staff', 'hispanic_senior_staff', 'middle_eastern_senior_staff', 'native_american_senior_staff', 'pacific_islander_senior_staff', 'white_senior_staff', 'multi_racial_senior_staff', 'other_ethnicity_senior_staff', 'race_decline_to_state_senior_staff', 'race_unknown_senior_staff','female_senior_staff','male_senior_staff','non_binary_senior_staff','gender_decline_to_state_senior_staff','gender_unknown_senior_staff','trans_senior_staff','cis_senior_staff']},
+    {'control_column': 'total_board', 'columns': ['female_board', 'male_board', 'non_binary_board', 'gender_decline_to_state_board', 'gender_unknown_board', 'trans_board', 'cis_board', 'asian_board', 'black_board', 'hispanic_board', 'middle_eastern_board', 'native_american_board', 'pacific_islander_board', 'white_board', 'multi_racial_board', 'other_ethnicity_board']},
+    {'control_column': 'total_staff', 'columns': ['asian_staff','black_staff','hispanic_staff','middle_eastern_staff','native_american_staff','pacific_islander_staff','white_staff','multi_racial_staff','other_ethnicity_staff','race_decline_to_state_staff','race_unknown_staff','female_staff','male_staff','non_binary_staff','gender_decline_to_state_staff','gender_unknown_staff','trans_staff','cis_staff']}
 ]
 
 calculate_percentages(input_file, output_file, control_column_pairs)
