af57682e8e0880eda05a2a710ef6003361e68da4
duffdl <duffdl@rose-hulman.edu>
2023-11-29 14:50:52 -0500

    finalizing data and trimming columns

--- python_scripts/main.py
+++ python_scripts/main.py
@@ -1,3 +1,7 @@
+from multiprocessing import Pool
+import copy
+import matplotlib.pyplot as plt
+import time
 from difflib import SequenceMatcher
 import requests
 import pandas as pd
@@ -6,6 +10,7 @@ from xml_parsing import get_990_info_for_company
 from multiprocessing import Pool, cpu_count
 from io import StringIO
 from concurrent.futures import ThreadPoolExecutor
+from percentage import calculate_percentages
 
 
 propublica_api_url = "https://projects.propublica.org/nonprofits/api/v2/search.json"
@@ -33,7 +38,8 @@ def get_best_matching_eins_from_company_name(company_name):
         similarity = string_similarity(company_name, org_name)
         similarities[i] = similarity
 
-    sorted_similarities = sorted(similarities.items(), key=lambda x: x[1], reverse=True)
+    sorted_similarities = sorted(
+        similarities.items(), key=lambda x: x[1], reverse=True)
 
     return list(
         map(
@@ -68,11 +74,12 @@ def fetch_org_info(ein):
     propublica_org_api_url = (
         "https://projects.propublica.org/nonprofits/api/v2//organizations/"
     )
-    propub_info = requests.get(url=propublica_org_api_url + f"{ein}.json").json()
+    propub_info = requests.get(
+        url=propublica_org_api_url + f"{ein}.json").json()
     return propub_info
 
 
-def get_propublica_top_df(min_revenue):
+def get_top_df_990(min_revenue):
     base_url = "https://www.irs.gov/pub/irs-soi/"
     file_names = ["eo1.csv", "eo2.csv", "eo3.csv", "eo4.csv"]
     dataframes = {}
@@ -108,68 +115,70 @@ def add_info_for_org(row):
         return None
 
 
-import time
-import matplotlib.pyplot as plt
-import copy
-from multiprocessing import Pool
-
 if __name__ == "__main__":
     absolute_start = time.time()
 
-    # # Get Candid >= 50 employees
+    # Get Candid >= 50 employees
     candid_top_df = get_candid_top_df()
 
-    # # Get info from 990 for Candid >= 50 employees
-
-    # # Non-threaded
-    # # # data = [add_info_for_org(row) for _, row in candid_top_df.iterrows()]
-    # # # candid_top_df = pd.DataFrame([r for r in data if r is not None])
-
-    # # Multi-processing
+    # Multi-processing
     num_procs = 10
 
     with Pool(num_procs) as pool:
-        data = pool.map(add_info_for_org, [row for _, row in candid_top_df.iterrows()])
+        data = pool.map(add_info_for_org, [
+                        row for _, row in candid_top_df.iterrows()])
 
     candid_top_df = pd.DataFrame([r for r in data if r is not None])
 
-    # # Get min revenue
+    # Get min revenue
     min_revenue = candid_top_df["total_revenue"].min()
     # print(f"Min revenue: {min_revenue}")
 
-    # Use min revenue to get all ProPublica with revenue >= this
-    # propublica_top_df = pd.read_csv("ProPublica-Top.csv").rename(columns={"EIN": "ein"}).loc[:, ["ein"]]
-    propublica_top_df = (
-        get_propublica_top_df(min_revenue=min_revenue)
+    # Use min revenue to get all 990s with revenue >= this
+    top_df_990 = (
+        get_top_df_990(min_revenue=min_revenue)
         .rename(columns={"EIN": "ein"})
         .loc[:, ["ein"]]
     )
 
     with Pool(num_procs) as pool:
         data = pool.map(
-            add_info_for_org, [row for _, row in propublica_top_df.iterrows()]
+            add_info_for_org, [row for _, row in top_df_990.iterrows()]
         )
 
     absolute_end = time.time()
 
     print(f"ALL DONE!!!! (took {absolute_end-absolute_start} seconds)")
 
-    propublica_top_df = pd.DataFrame([r for r in data if r is not None])
+    top_df_990 = pd.DataFrame([r for r in data if r is not None])
 
-    propublica_top_df.to_csv("990-Top-11-23.csv")
-    candid_top_df.to_csv("Candid-Top-11-23.csv")
-    # propublica_top_df.drop(columns=['ICO', 'org_name', 'ZIP',
-    #                                 'GROUP', 'SUBSECTION', 'AFFILIATION',
-    #                                   'CLASSIFICATION', 'RULING', 'DEDUCTIBILITY',
-    #                                     'FOUNDATION', 'ACTIVITY', 'ORGANIZATION',
-    #                                     'STATUS', 'TAX_PERIOD', 'ASSET_CD', 'INCOME_CD',
-    #                                       'FILING_REQ_CD', 'PF_FILING_REQ_CD', 'ACCT_PD',
-    #                                         'ASSET_AMT', 'INCOME_AMT', 'REVENUE_AMT', 'NTEE_CD', 'SORT_NAME'])
+    top_df_990 = top_df_990.reset_index(drop=True)
+    candid_top_df = candid_top_df.reset_index(drop=True)
+
+    top_df_990.to_csv("990-Top-11-29.csv", index=False)
+    candid_top_df = candid_top_df.drop(columns=["Unnamed: 0", "dei_last_updated", "race_leader", "gender1_leader", "gender2_leader", "orientation_leader",
+                                                "disability_status_leader", "race_coleader", "gender1_coleader", "gender2_coleader", "orientation_coleader",
+                                                "disability_status_coleader",
+                                                "gender2_decline_to_state_board", "gender2_unknown_board", "lgbtqia_board", "straight_board",
+                                                "orientation_decline_to_state_board", "orientation_unknown_board", "with_disability_board",
+                                                "without_disability_board", "disability_decline_to_state_board", "disability_unknown_board",
+
+                                                "gender2_decline_to_state_senior_staff", "gender2_unknown_senior_staff", "lgbtqia_senior_staff", "straight_senior_staff",
+                                                "orientation_decline_to_state_senior_staff", "orientation_unknown_senior_staff", "with_disability_senior_staff",
+                                                "without_disability_senior_staff", "disability_decline_to_state_senior_staff", "disability_unknown_senior_staff",
+
+
+                                                "gender2_decline_to_state_staff", "gender2_unknown_staff", "lgbtqia_staff", "straight_staff",
+                                                "orientation_decline_to_state_staff", "orientation_unknown_staff", "with_disability_staff",
+                                                "without_disability_staff", "disability_decline_to_state_staff", "disability_unknown_staff"])
+    candid_top_df.to_csv("Candid-Top-11-29.csv", index=False)
+    calculate_percentages("Candid-Top-11-29.csv",
+                          "Candid-Top-11-29-with-Percentages.csv")
 
     # Merge by EIN
     # candid_top_df['ein'] = candid_top_df['ein'].map(lambda x : x.replace("-", "")).astype("int")
-    # candid_with_propublica = propublica_top_df.merge(candid_top_df, left_on='EIN', right_on='ein', how='inner')
+    # candid_with_propublica = top_df_990.merge(candid_top_df, left_on='EIN', right_on='ein', how='inner')
 
     # candid_top_df.to_csv("Candid-Top.csv")
-    # propublica_top_df.to_csv("ProPublica-Top.csv")
+    # top_df_990.to_csv("ProPublica-Top.csv")
     # candid_with_propublica.to_csv("Candid-Top-With-ProPublica-Info.csv")
