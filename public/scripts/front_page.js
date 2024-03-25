c0a4b5a9edf04a178e58549b9d8131304853ba03
rhit-duffdl <duffdl@rose-hulman.edu>
2024-03-25 12:34:16 -0400

    pagination and search baseline functionality done

--- public/scripts/front_page.js
+++ public/scripts/front_page.js
@@ -1,6 +1,3 @@
-let data_candid;
-let data_990;
-
 // var lastScrollTop;
 // navbar = document.getElementById('navbar');
 // window.addEventListener('scroll',function(){
@@ -13,6 +10,75 @@ let data_990;
 // }
 // lastScrollTop = scrollTop;
 // });
+
+
+let data_candid;
+let data_990;
+
+let curr_rank = 0;
+let currentPage = 1;
+const recordsPerPage = 50;
+let filteredData = [];
+let currentData = [];
+let allData = [];
+
+function changePage(increment) {
+    const numPages = Math.ceil(data_candid.length / recordsPerPage);
+    lastPage = currentPage;
+    currentPage += increment;
+
+    if (currentPage < 1) currentPage = 1;
+    if (currentPage > numPages) currentPage = numPages;
+
+    curr_rank = currentPage * 50 - 50;
+
+    document.getElementById('currentPage').textContent = currentPage;
+
+    const startIdx = (currentPage - 1) * recordsPerPage;
+    const endIdx = startIdx + recordsPerPage;
+    currentData = allData.slice(startIdx, endIdx);
+
+    renderData();
+}
+
+function renderData() {
+    const container = document.querySelector('.data-rows');
+    container.innerHTML = ``;
+
+    let toRender = [];
+
+    if (filteredData.length > 0) {
+        toRender = filteredData;
+    } else {
+        toRender = currentData;
+    }
+
+    toRender.forEach(function (org) {
+        curr_rank++;
+        let score = org.total_score;
+
+        const orgHtml = `
+            <div class="org-row" data-ein="${org.ein}">
+                <div class="input-rank">#${curr_rank}</div>
+                <div class="input-org">${org.org_name}</div>
+                <div class="score-container">
+                    <div class="rectangle" style="width: ${score}%"></div>
+                </div>
+                <div class="score">${score}</div>
+            </div>
+        `;
+
+        container.insertAdjacentHTML('beforeend', orgHtml);
+
+    });
+    document.querySelectorAll('.org-row').forEach(row => {
+        row.addEventListener('click', function () {
+            const ein = this.getAttribute('data-ein');
+            window.location.href = `${window.location.href}breakdown.html?ein=${ein}`;
+        });
+    });
+}
+
 
 Promise.all([
     d3.csv('data/Candid-Trimmed.csv'),
@@ -36,52 +102,29 @@ Promise.all([
     console.log(files[1]);
     data_990 = files[1];
 
-    let curr_rank = 0;
+    allData = data_candid;
 
-    data_candid.forEach(function (org) {
-        curr_rank++;
-        let score = org.total_score;
-
-        const orgHtml = `
-            <div class="org-row" data-ein="${org.ein}">
-                <div class="input-rank">#${curr_rank}</div>
-                <div class="input-org">${org.org_name}</div>
-                <div class="score-container">
-                    <div class="rectangle" style="width: ${score}%"></div>
-                </div>
-                <div class="score">${score}</div>
-            </div>
-        `;
-
-        document.querySelector(".scoreboard").insertAdjacentHTML('beforeend', orgHtml);
-
-    });
-
-    document.querySelectorAll('.org-row').forEach(row => {
-        row.addEventListener('click', function () {
-            const ein = this.getAttribute('data-ein');
-            window.location.href = `${window.location.href}breakdown.html?ein=${ein}`;
-        });
-    });
-
-
-
-    const orgHtml = `
-    <div class="org-row">
-        <div class="input-rank">#1</div>
-        <div class="input-org" id="specificOrganization">org A</div>
-        <div class="score-container">
-          
-          <div class="rectangle"></div>
-        </div>
-        <div class="score">95</div>
-
-      </div>
-      `
-
-
+    changePage(0);
 
 }).catch(function (error) {
     console.error('Error loading data: ', error);
 });
 
+document.getElementById('searchBox').addEventListener('input', function () {
+    const query = this.value.toLowerCase();
+    filteredData = currentData.filter(d => d.org_name.toLowerCase().includes(query));
+    if (filteredData.length <= 0 && query !== '') {
+        this.classList.add('no-results');
+        this.disabled = true;
+        
+        setTimeout(() => {
+            this.disabled = false;
+            this.focus();
+        }, 500);
+    } else {
+        this.classList.remove('no-results');
+        this.disabled = false;
+    }
+    currentPage = 1;
+    changePage(0);
+});
